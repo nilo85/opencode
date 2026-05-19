fa2d007fb1fa463499d771756b556178f15bae6c
Niklas Lönn <niklas.lonn@gmail.com>
2026-05-19 16:41:43 +0200

    chore: relax bun version requirement for nix build compatibility

--- packages/script/src/index.ts
+++ packages/script/src/index.ts
@@ -11,7 +11,7 @@ if (!expectedBunVersion) {
 }
 
 // relax version requirement
-const expectedBunVersionRange = `^${expectedBunVersion}`
+const expectedBunVersionRange = `>=1.3.3`
 
 if (!semver.satisfies(process.versions.bun, expectedBunVersionRange)) {
   throw new Error(`This script requires bun@${expectedBunVersionRange}, but you are using bun@${process.versions.bun}`)
